b564e8aced11316d9f22b9002564bf30570a9f31
manseerat21 <smanseeratsinghgill21@gmail.com>
2023-10-09 21:23:52 +0530

    modified:   app/components/Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,40 +1,38 @@
-
 "use client"
-import Image from 'next/image'
-import React, { useEffect } from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
+import Image from 'next/image';
+import React, { useEffect } from 'react';
+import { useSession, signIn } from "next-auth/react";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
-import { HiSearch,HiBell,HiChat } from "react-icons/hi";
-import app from '../Shared/firebaseConfig'
 import { useRouter } from 'next/navigation';
+import app from '../Shared/firebaseConfig';
 
 function Header() {
   const { data: session } = useSession();
-  const router=useRouter();
+  
+  const router = useRouter();
   const db = getFirestore(app);
 
-  useEffect(()=>{
+  useEffect(() => {
     saveUserInfo();
-  },[session])
+  }, []);
 
-  const saveUserInfo=async()=>{
-    if(session?.user)
-    {
-      await setDoc(doc(db, "user", session.user.email), {
+  const saveUserInfo = async () => {
+    if (session?.user) {
+      // @ts-ignore
+      const userDocRef = doc(db, "user", session.user.email)
+      await setDoc(userDocRef, {
         userName: session.user.name,
         email: session.user.email,
-        userImage: session.user.image
+        userImage: session.user.image,
       });
     }
-  }
+  };
 
-  const onCreateClick=()=>{
-    if(session)
-    {
-      router.push('/upload')
-    }
-    else{
-      signIn()
+  const onCreateClick = () => {
+    if (session) {
+      router.push('/upload');
+    } else {
+      signIn();
     }
   }
 
@@ -61,10 +59,12 @@ function Header() {
   >
     Create
   </button>
-  <div className='ml-auto'> {/* Use ml-auto to push content to the far right */}
+  <div className='ml-auto'>
     {session?.user ? (
       <Image
+      // @ts-ignore
         src={session.user.image}
+        // @ts-ignore
         onClick={() => router.push('/' + session.user.email)}
         alt='user-image'
         width={60}
