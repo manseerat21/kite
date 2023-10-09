2e58d923d5053987d721c69b5f99189136be2fec
manseerat21 <smanseeratsinghgill21@gmail.com>
2023-10-09 22:30:51 +0530

    modified:   app/Shared/firebaseConfig.js 	modified:   app/components/PinDetail/PinInfo.jsx 	modified:   app/components/UploadImage.jsx

--- app/components/PinDetail/PinInfo.jsx
+++ app/components/PinDetail/PinInfo.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import UserTag from '../UserTag'
+import React from 'react';
+import UserTag from '../UserTag';
 
-function PinInfo({pinDetail}) {
-  const user={
-    name:pinDetail.userName,
-    email:pinDetail.email,
-    image:pinDetail.userImage
-  }
+function PinInfo({ pinDetail }) {
+  const user = {
+    name: pinDetail.userName,
+    email: pinDetail.email,
+    image: pinDetail.userImage,
+  };
+
+  const openLink = () => {
+    if (typeof window !== 'undefined') {
+      window.open(`https://${pinDetail.link}`);
+    }
+  };
+
   return (
     <div>
-      <h2 className='text-[30px] font-bold'>{pinDetail.title}</h2>
+      <h2 className="text-[30px] font-bold">{pinDetail.title}</h2>
       <UserTag user={user} />
       <h2>{pinDetail.desc}</h2>
-      <button className='p-2 bg-[#e9e9e9] px-5 text-[23px]
-      mt-10 rounded-full hover:scale-105 transition-all'
-      onClick={()=>window.open(`https://${pinDetail.link}`)}>Open Url</button>
+      <button
+        className="p-2 bg-[#e9e9e9] px-5 text-[23px] mt-10 rounded-full hover:scale-105 transition-all"
+        onClick={openLink}
+      >
+        Open Url
+      </button>
     </div>
-  )
+  );
 }
 
-export default PinInfo
+export default PinInfo;
