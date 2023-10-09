35f6606e86d111df5adfa99713c0b50cf8b0929f
manseerat21 <smanseeratsinghgill21@gmail.com>
2023-10-09 20:44:25 +0530

    modified:   app/Provider.tsx

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { SessionProvider } from 'next-auth/react'
-import React from 'react'
+import React, { ReactNode } from 'react';
 
-function Provider({children}) {
+interface ProviderProps {
+  children: ReactNode;
+}
+
+function Provider({ children }: ProviderProps) {
   return (
     <SessionProvider>
       {children}
     </SessionProvider>
-  )
+  );
 }
 
 export default Provider;
