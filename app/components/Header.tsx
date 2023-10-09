bed86612b9f408414feb09419548c5dc962a6e8e
manseerat21 <smanseeratsinghgill21@gmail.com>
2023-10-09 20:35:04 +0530

    modified:   app/components/Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -40,43 +40,48 @@ function Header() {
 
   
   return (
-    <div className='flex justify-between 
-     gap-3 md:gap-2 items-center p-6 '>
-        <Image src='/logo.svg' alt='logo'
-        width={60} height={60} onClick={()=>router.push('/')}
-        className='
-        cursor-pointer'/>
-        <button className='bg-black
-         text-white p-3 px-6 rounded-full
-         text-[25px]
-          hidden md:block' onClick={()=>router.push('/')}>Home</button>
-        <button className='font-semibold p-3 px-6
-         rounded-full text-[25px]' 
-         onClick={()=>onCreateClick()}>Create</button>
-        <div className='bg-[#e9e9e9] p-3 px-6
-         gap-3 items-center rounded-full w-full hidden md:flex'>
-        <HiSearch className='text-[34px] 
-        text-gray-500'/>
-        <input type="text" placeholder='Search'
-        className='bg-transparent outline-none w-full text-[25px]' />
-       
-        </div>
-        <HiSearch className='text-[25px] 
-        text-gray-500 md:hidden'/>
-        <HiBell className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>
-        <HiChat className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>
-      {session?.user?  
-      <Image src={session.user.image}
-      onClick={()=>router.push('/'+session.user.email)}
-      alt='user-image' width={60} height={60}
-        className='rounded-full cursor-pointer'/>:
+    <div className='flex gap-3 md:gap-2 p-6'>
+  <Image
+    src='/logo.svg'
+    alt='logo'
+    width={60}
+    height={60}
+    onClick={() => router.push('/')}
+    className='cursor-pointer'
+  />
+  <button
+    className='bg-black text-white p-3 px-6 rounded-full text-[25px] hidden md:block'
+    onClick={() => router.push('/')}
+  >
+    Home
+  </button>
+  <button
+    className='font-semibold p-3 px-6 rounded-full text-[25px]'
+    onClick={() => onCreateClick()}
+  >
+    Create
+  </button>
+  <div className='ml-auto'> {/* Use ml-auto to push content to the far right */}
+    {session?.user ? (
+      <Image
+        src={session.user.image}
+        onClick={() => router.push('/' + session.user.email)}
+        alt='user-image'
+        width={60}
+        height={60}
+        className='rounded-full cursor-pointer'
+      />
+    ) : (
+      <button
+        className='font-semibold p-2 px-4 rounded-full'
+        onClick={() => signIn()}
+      >
+        Login
+      </button>
+    )}
+  </div>
+</div>
 
-        <button className='font-semibold p-2 px-4 rounded-full'
-         onClick={() => signIn()}>Login</button>}
-
-
-
-    </div>
   )
 }
 
