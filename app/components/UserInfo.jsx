a6c683a3c906eb49ea65bf02e1a4a64760e12047
manseerat21 <smanseeratsinghgill21@gmail.com>
2023-10-10 04:03:20 +0530

    modified:   app/components/UserInfo.jsx

--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -21,13 +21,13 @@ function UserInfo({userInfo}) {
         <h2 className='text-[30px]
         font-semibold'>{userInfo.userName}</h2>
         <h2 className='text-gray-400'>{userInfo.email}</h2>
-        <div className='flex gap-4'>
-        <button className='bg-gray-200
-         p-2 px-3 font-semibold mt-5 rounded-full'>Share</button>
-        {session?.user.email== userInfo.email? <button className='bg-gray-200
-         p-2 px-3 font-semibold mt-5 rounded-full'
-         onClick={()=>onLogoutClick()}>Logout</button>:null}
-      </div>
+        <div className='flex gap-4 mb-5'>
+          <button className='bg-gray-200
+          p-2 px-3 font-semibold mt-5 rounded-full'>Share</button>
+          {session?.user.email== userInfo.email? <button className='bg-gray-200
+          p-2 px-3 font-semibold mt-5 rounded-full'
+          onClick={()=>onLogoutClick()}>Logout</button>:null}
+        </div>
     </div>
   )
 }
